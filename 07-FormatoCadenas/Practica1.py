9ba36ed862d5e512196a4938bb89c392962e5671
Cristxna <acpachecor@utn.edu.ec>
2024-05-29 21:58:21 -0500

    FormatoCadenas

---
+++ 07-FormatoCadenas/Practica1.py
@@ -0,0 +1,3 @@
+nombre_asociado = "Ana Cristina"
+numero_asociado = 40823
+print("Estimado/a {}, su número de asociado es: {}". format(nombre_asociado,numero_asociado))
---
+++ 07-FormatoCadenas/Practica2.py
@@ -0,0 +1,5 @@
+puntos_nuevos = 4
+puntos_antiguos = 16
+puntos_totales = puntos_nuevos + puntos_antiguos
+
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
