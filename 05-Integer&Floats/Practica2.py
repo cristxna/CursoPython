eadba883f75dad0add3928374cd0e20b7cc4451d
Cristxna <acpachecor@utn.edu.ec>
2024-05-29 20:55:23 -0500

    Integer&Float

---
+++ 05-Integer&Floats/Practica2.py
@@ -0,0 +1,2 @@
+num_decimal = 12.8
+print(type(num_decimal))
---
+++ 05-Integer&Floats/app.py
@@ -0,0 +1,12 @@
+mi_numero = 1
+#print(mi_numero)
+
+#verificar el tipo de dato
+#print(type(mi_numero))
+
+#ejemplo
+edad = input("Dime tu edad: ")
+print("Tu edad es" + " " + edad)
+
+nuevaEdad = edad + edad
+print(nuevaEdad)
